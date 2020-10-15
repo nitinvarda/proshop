9773d7fff3bf92f2ab40fa0848575c2a0c1c79e6
Nitin <nitinvardha09@gmail.com>
2020-10-15 23:54:46 +0530

    preparing for deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import OrderListScreen from './screens/OrderListScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/admin/userlist" component={UserListScreen} />
+            <Route path="/admin/orderlist" component={OrderListScreen} />
+            <Route path="/admin/productlist" exact component={ProductListScreen} />
+            <Route path="/admin/productlist/:pageNumber" exact component={ProductListScreen} />
+            <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+            <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+            <Route path="/search/:keyword" component={HomeScreen} exact />
+            <Route path="/page/:pageNumber" component={HomeScreen} />
+            <Route path="/search/:keyword/page/:pageNumber" component={HomeScreen} />
+            <Route path="/" exact component={HomeScreen} />
+
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
